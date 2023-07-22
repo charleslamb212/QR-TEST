2e6937cf857c7f79c5d930b0950d893f7325f9e2
charleslamb212 <charlesandrewlamb@gmail.com>
2023-07-22 14:32:13 -0700

    nav

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 function generateQRCode() {
-    const inputText = document.getElementById("inputText").value;
+  const inputText = document.getElementById("inputText").value;
 
-    // Check if the user entered some text
-    if (inputText.trim() === "") {
-        alert("Please enter some text or URL to generate the QR code.");
-        return;
-    }
+  // Check if the user entered some text
+  if (inputText.trim() === "") {
+    alert("Please enter some text or URL to generate the QR code.");
+    return;
+  }
 
-    // Generate QR code
-    const qrcodeDiv = document.getElementById("qrcode");
-    qrcodeDiv.innerHTML = "";
+  // Generate QR code
+  const qrcodeDiv = document.getElementById("qrcode");
+  qrcodeDiv.innerHTML = "";
 
-    const qrcode = new QRCode(qrcodeDiv, {
-        text: inputText,
-        width: 256,
-        height: 256,
-    });
+  const qrcode = new QRCode(qrcodeDiv, {
+    text: inputText,
+    width: 256,
+    height: 256,
+    margin: 0.5,
+  });
+}
+
+function toggleMenu() {
+  var navItems = document.getElementById("navItems");
+  if (navItems.style.display === "flex") {
+    navItems.style.display = "none";
+  } else {
+    navItems.style.display = "flex";
+  }
 }
