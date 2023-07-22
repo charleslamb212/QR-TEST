6d6dc16940660a52ce5fcc74d0e42b83f2f63493
charleslamb212 <charlesandrewlamb@gmail.com>
2023-07-22 13:07:47 -0700

    added logic

---
+++ app.js
@@ -0,0 +1,19 @@
+function generateQRCode() {
+    const inputText = document.getElementById("inputText").value;
+
+    // Check if the user entered some text
+    if (inputText.trim() === "") {
+        alert("Please enter some text or URL to generate the QR code.");
+        return;
+    }
+
+    // Generate QR code
+    const qrcodeDiv = document.getElementById("qrcode");
+    qrcodeDiv.innerHTML = "";
+
+    const qrcode = new QRCode(qrcodeDiv, {
+        text: inputText,
+        width: 256,
+        height: 256,
+    });
+}
