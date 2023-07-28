db3c2c03cabfd6c394a1d52a49b789b286d16fad
charleslamb212 <charlesandrewlamb@gmail.com>
2023-07-27 22:39:57 -0700

    setup

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const QRCode = require('qrcode');
-const bodyParser = require('body-parser');
+const express = require("express");
+const QRCode = require("qrcode");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = 3000;
@@ -9,21 +9,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // API endpoint to generate QR code
-app.post('/generateQRCode', (req, res) => {
+app.post("/generateQRCode", (req, res) => {
   const { ssid, password, security } = req.body;
-
-  // Replace these placeholder values with your actual Wi-Fi credentials
-  const wifiCredentials = {
-    ssid: "Your_Wi-Fi_SSID",
-    password: "Your_Wi-Fi_Password",
-    security: "WPA/WPA2", // Security type (e.g., WEP, WPA, WPA2)
-  };
 
   // Validate the input and handle errors
   // ...
 
   // Create the Wi-Fi login string
-  const wifiLoginString = `WIFI:T:${wifiCredentials.ssid};S:${wifiCredentials.password};P:${wifiCredentials.security};`;
+  const wifiLoginString = `WIFI:T:${ssid};S:${password};P:${security};`;
 
   // Generate QR code
   QRCode.toDataURL(wifiLoginString, (err, url) => {
