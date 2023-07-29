cc6e0878219df2e774df78609b58f65cbbeeff3a
charleslamb212 <charlesandrewlamb@gmail.com>
2023-07-28 22:45:00 -0700

    server.js

--- server.js
+++ server.js
@@ -1,34 +1,26 @@
 const express = require("express");
-const QRCode = require("qrcode");
-const bodyParser = require("body-parser");
+const qrcode = require("qrcode");
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// API endpoint to generate QR code
-app.post("/generateQRCode", (req, res) => {
+app.use(express.json());
+//API enpoint to generate QR code
+app.post("/generateQRCode", async (req, res) => {
   const { ssid, password, security } = req.body;
 
-  // Validate the input and handle errors
-  // ...
+  // Generate the Wi-Fi configuration string
+  const wifiConfig = `WIFI:T:${security};S:${ssid};P:${password};;`;
 
-  // Create the Wi-Fi login string
-  const wifiLoginString = `WIFI:T:${ssid};S:${password};P:${security};`;
-
-  // Generate QR code
-  QRCode.toDataURL(wifiLoginString, (err, url) => {
-    if (err) {
-      console.error("Error generating QR code:", err);
-      res.status(500).json({ error: "Error generating QR code" });
-    } else {
-      res.json({ qrCodeUrl: url });
-    }
-  });
+  try {
+    const qrCodeUrl = await qrcode.toDataURL(wifiConfig);
+    res.json({ qrCodeUrl });
+  } catch (err) {
+    console.error("Error generating QR code:", err);
+    res.status(500).json({ error: "Error generating QR code" });
+  }
 });
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on port ${port}`);
 });
