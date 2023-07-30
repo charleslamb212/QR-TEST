0d2cb8beb9d9d9e7ca53a52d71f2e2ce46dc43f6
charleslamb212 <charlesandrewlamb@gmail.com>
2023-07-30 13:27:32 -0700

    updated server.js

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const qrcode = require("qrcode");
+const path = require("path");
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+// Define a route for serving the index.html file
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
 //API enpoint to generate QR code
 app.post("/generateQRCode", async (req, res) => {
   const { ssid, password, security } = req.body;
