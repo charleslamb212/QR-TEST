db0c4757b514b6cbaf9df9d433404f1740098b97
charleslamb212 <charlesandrewlamb@gmail.com>
2023-08-11 21:36:23 -0700

    dl btn

--- public/app.js
+++ public/app.js
@@ -1,17 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const wifiForm = document.getElementById("wifi-form");
-  const qrContainer = document.getElementById("qr-container");
+document.addEventListener('DOMContentLoaded', () => {
+  const wifiForm = document.getElementById('wifi-form');
+  const qrContainer = document.getElementById('qr-container');
 
-  wifiForm.addEventListener("submit", async (event) => {
+  wifiForm.addEventListener('submit', async (event) => {
     event.preventDefault();
 
     const ssid = event.target.ssid.value;
     const password = event.target.password.value;
 
-    const response = await fetch("/generate", {
-      method: "POST",
+    const response = await fetch('/generate', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({ ssid, password }),
     });
@@ -19,21 +19,21 @@ document.addEventListener("DOMContentLoaded", () => {
     const qrCodeHTML = await response.text();
     qrContainer.innerHTML = qrCodeHTML;
 
-    qrContainer.classList.add("show");
+    qrContainer.classList.add('show');
 
     // Add the download button dynamically
-    const qrImage = qrContainer.querySelector("img");
-    const downloadButton = document.createElement("button");
-    downloadButton.id = "download-btn";
-    downloadButton.className = "btn btn-secondary";
-    downloadButton.textContent = "Download QR Code";
+    const qrImage = qrContainer.querySelector('img');
+    const downloadButton = document.createElement('button');
+    downloadButton.id = 'download-btn';
+    downloadButton.className = 'btn btn-secondary';
+    downloadButton.textContent = 'Download QR Code';
     qrContainer.appendChild(downloadButton);
 
     // Add event listener to the download button
-    downloadButton.addEventListener("click", () => {
-      const downloadLink = document.createElement("a");
+    downloadButton.addEventListener('click', () => {
+      const downloadLink = document.createElement('a');
       downloadLink.href = qrImage.src;
-      downloadLink.download = "wifi_qr_code.png";
+      downloadLink.download = 'wifi_qr_code.png';
       downloadLink.click();
     });
   });
