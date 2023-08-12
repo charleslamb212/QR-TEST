969957a0d219a680348d4dc25a79d416437a3d62
charleslamb212 <charlesandrewlamb@gmail.com>
2023-08-11 21:08:43 -0700

    init commit

--- public/app.js
+++ public/app.js
@@ -1,78 +1,40 @@
-// const express = require("express");
-// const app = express();
-// const port = 3000;
+document.addEventListener("DOMContentLoaded", () => {
+  const wifiForm = document.getElementById("wifi-form");
+  const qrContainer = document.getElementById("qr-container");
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
+  wifiForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
 
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`);
-// });
-// end express setup
-// function generateQRCode() {
-//   const inputText = document.getElementById("inputText").value;
+    const ssid = event.target.ssid.value;
+    const password = event.target.password.value;
 
-//    Check if the user entered some text
-//   if (inputText.trim() === "") {
-//     alert("Please enter some text or URL to generate the QR code.");
-//     return;
-//   }
-
-//    Generate QR code
-//   const qrcodeDiv = document.getElementById("qrcode");
-//   qrcodeDiv.innerHTML = "";
-
-//   const qrcode = new QRCode(qrcodeDiv, {
-//     text: inputText,
-//     width: 256,
-//     height: 256,
-//     margin: 0.5,
-//   });
-// }
-
-// function toggleMenu() {
-//   var navItems = document.getElementById("navItems");
-//   if (navItems.style.display === "flex") {
-//     navItems.style.display = "none";
-//   } else {
-//     navItems.style.display = "flex";
-//   }
-// }
-
-const wifiForm = document.getElementById("wifiForm");
-const qrcodeDiv = document.getElementById("qrcode");
-
-wifiForm.addEventListener("submit", async (event) => {
-  event.preventDefault();
-  console.log("Form submitted!"); // Add this line
-
-  const ssid = document.getElementById("ssid").value.trim();
-  const password = document.getElementById("password").value.trim();
-  const security = document.getElementById("security").value.trim();
-
-  // Validate the input and handle errors
-  // ...
-
-  try {
-    const response = await fetch("/generateQRCode", {
+    const response = await fetch("/generate", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ ssid, password, security }),
+      body: JSON.stringify({ ssid, password }),
     });
 
-    if (response.ok) {
-      const data = await response.json();
-      const qrCodeUrl = data.qrCodeUrl;
-      qrcodeDiv.innerHTML = `<img src="${qrCodeUrl}" alt="Wi-Fi QR Code">`;
-    } else {
-      console.error("Error generating QR code:", response.status);
-      qrcodeDiv.innerHTML = "<p>Error generating QR code.</p>";
-    }
-  } catch (err) {
-    console.error("Error generating QR code:", err);
-    qrcodeDiv.innerHTML = "<p>Error generating QR code.</p>";
-  }
+    const qrCodeHTML = await response.text();
+    qrContainer.innerHTML = qrCodeHTML;
+
+    qrContainer.classList.add("show");
+
+    // Add the download button dynamically
+    const qrImage = qrContainer.querySelector("img");
+    const downloadButton = document.createElement("button");
+    downloadButton.id = "download-btn";
+    downloadButton.className = "btn btn-secondary";
+    downloadButton.textContent = "Download QR Code";
+    qrContainer.appendChild(downloadButton);
+
+    // Add event listener to the download button
+    downloadButton.addEventListener("click", () => {
+      const downloadLink = document.createElement("a");
+      downloadLink.href = qrImage.src;
+      downloadLink.download = "wifi_qr_code.png";
+      downloadLink.click();
+    });
+  });
 });
--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-const express = require('express');
-const qrcode = require('qrcode');
+const express = require("express");
+const qrcode = require("qrcode");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.post('/generate', (req, res) => {
+app.post("/generate", (req, res) => {
   const { ssid, password } = req.body;
   const wifiConfig = `WIFI:T:WPA;S:${ssid};P:${password};;`;
 
